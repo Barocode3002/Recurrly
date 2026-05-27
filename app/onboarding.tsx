8a2863d6c79f90e96c77130d55bcf78c359cf21f
Mohsen Barokha <mohsenbarokha@192.168.1.15>
2026-05-27 14:18:40 +0300

    first commit

---
+++ app/onboarding.tsx
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const onboarding = () => {
+  return (
+    <View>
+      <Text className='text-center justify-center items-center mt-50 text-success text-[34px]'>onboarding</Text>
+    </View>
+  )
+}
+
+export default onboarding
